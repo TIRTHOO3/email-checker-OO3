e647d9d4f0ea9a0447c9005e91d65384df30e66c
TIRTHOO3 <chaudharitirthm@gmail.com>
2025-04-08 08:03:26 +0530

    Added risk detection logic using CSV domain list

--- app.py
+++ app.py
@@ -49,5 +49,4 @@ def index():
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))  # Default to 5000 locally
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=10000)
